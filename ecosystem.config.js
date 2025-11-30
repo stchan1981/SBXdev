c31b0af2dd68d229845fcd88330c2ac48b550efb
stchan1981 <handson.chan@gmail.com>
2025-11-30 21:45:26 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     watch: false,
     max_memory_restart: '1G',
     env: {
-      PORT: process.env.PORT || 3002,  // 动态从 env 取，默认 3002
+      PORT: process.env.PORT || 3002,
       UUID: process.env.UUID || 'd5646919-1638-4dc6-9799-d795595c6b65',
       ARGO_AUTH: process.env.ARGO_AUTH || ''
     },
